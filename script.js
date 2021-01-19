f3c4ad94cfa8bf5a5c0f59d99bcfc92a8cd061bd
Himanshu Singh Parmar <hp2146@gmail.com>
2021-01-20 04:08:18 +0530

    initial commit5

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-balance = Math.round(Math.random() * 1000);
+let balance = Math.round(Math.random() * 1000) - 500;
 document.querySelector(".balance").textContent = `You have left ${balance} rs`;
-
+let x = 0;
 document.querySelector(".debit").addEventListener("click", function () {
   x = Number(document.querySelector(".value").value);
   balance = balance - x;
+  balance > 0
+    ? (document.querySelector(".balance").style.color = "blue")
+    : (document.querySelector(".balance").style.color = "red");
   document.querySelector(
     ".balance"
   ).textContent = `You have left ${balance} rs`;
 });
 document.querySelector(".credit").addEventListener("click", function () {
   x = Number(document.querySelector(".value").value);
-
   balance = balance + x;
+  balance > 0
+    ? (document.querySelector(".balance").style.color = "blue")
+    : (document.querySelector(".balance").style.color = "red");
   document.querySelector(
     ".balance"
   ).textContent = `You have left ${balance} rs`;
 });
+balance > 0
+  ? (document.querySelector(".balance").style.color = "blue")
+  : (document.querySelector(".balance").style.color = "red");
+document.querySelector(".balance");
